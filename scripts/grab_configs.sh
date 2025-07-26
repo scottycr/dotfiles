3ab0d7d0fa6f33a36a69ebdb613d5f0fe9545783
Theodore Corrello-Reiser <59662884+scottycr@users.noreply.github.com>
2025-07-26 13:32:30 -0600

    Refactored script to use functions so I don't have to excessively modify the script each time I add a new config category.

--- scripts/grab_configs.sh
+++ scripts/grab_configs.sh
@@ -1,175 +1,136 @@
 #!/bin/bash
 
+# Checks if bash version is compatible
+if (( BASH_VERSINFO[0] < 4 || ( BASH_VERSINFO[0] == 4 && BASH_VERSINFO[1] < 3 ) )); then
+    echo "This script requires Bash 4.3 or newer." >&2
+    exit 1
+fi
+
+
 # === START GLOBALS ===
-config_dir="$HOME/.config"
+local_config_dir="$HOME/.config"
 repo_config_dir="../configs"
 config_lists_dir="../config_lists"
-all_found_dirs=()
 # === END GLOBALS ===
+
+
+# === START FUNCTIONS ===
+load_config_list() {
+    local config_list_file="$1"
+    local -n config_list="$2"
+    config_list=()
+    local config_category="$3"
+    
+    # Checks to see if the provided configs list file exists
+    if [ ! -f "$config_list_file" ]; then
+        echo "Could not find $config_list_file in the config lists directory!"
+        echo "This script will not be able to find any $config_category configs."
+        return 1
+    fi
+
+    mapfile -t config_list < "$config_list_file"
+}
+
+load_config_dirs() {
+    local -n found_dirs="$1"
+    local -n config_dirs="$2"
+    local -n found_config_dirs="$3"
+    found_config_dirs=()
+
+    for found_dir in "${found_dirs[@]}"; do
+        # Remove trailing slash
+        found_dir="${found_dir%/}"
+        # Extract just the folder name
+        found_dir="${found_dir##*/}"
+
+        for config_dir in "${config_dirs[@]}"; do
+            if [[ "$found_dir" == "$config_dir" ]]; then
+                found_config_dirs+=("$found_dir")
+            fi
+        done
+    done
+}
+
+copy_config_files() {
+    local -n found_config_dirs="$1"
+    local config_category="$2"
+
+    if [ ${#found_config_dirs[@]} -eq 0 ]; then 
+        echo "No editor configs found!"
+    else
+        echo "Found editor config directories: ${found_config_dirs[@]}"
+        
+        while true; do
+            read -rp "Copy current editor configs over to repository: (y/N) " copy_config
+            # Defaults to no if user does not provide any input
+            if [ -z "$copy_config" ]; then
+                copy_config="n"
+            fi
+
+            copy_config="${copy_config,,}"
+            case $copy_config in [[yn]) break ;; *) echo "Invalid input, must choose y/n";; esac
+        done
+
+        if [ "$copy_config" = "y" ]; then
+            # Enable globbing for file checking
+            shopt -s nullglob dotglob
+
+            for dir in "${found_config_dirs[@]}"; do
+                echo "Grabbing current $dir config..."; 
+                # Grabbing all files in the current dir
+                files=("$local_config_dir/$dir"/*)
+                # Checks to see if there are no files in the current dir
+                if [ "${#files[@]}" -eq 0 ]; then
+                    echo "$dir is empty. Skipping!"
+                else
+                    cp --interactive --recursive --verbose "$local_config_dir/$dir" "$repo_config_dir/"
+                    echo "Done!"
+                fi
+            done
+
+            # Clean up to abide by best practices
+            shopt -u nullglob dotglob
+        else
+            echo "Skipping copying over $config_category configs!"
+        fi
+    fi
+    echo
+
+}
+# === END FUNCTIONS ===
+
 
 # === START CONFIG CATEGORIES ===
 editor_dirs_file="$config_lists_dir/editor_configs.txt"
-# Checks to see if the editor configs list file exists
-if [ -f "$editor_dirs_file" ]; then
-    # Checks to see if the mapfile command is available
-    if command -v mapfile >/dev/null 2>&1; then
-        mapfile -t editor_dirs < "$editor_dirs_file"
-    else
-        editor_dirs=()
-        while IFS= read -r line; do
-            editor_dirs+=("$line")
-        done < "$editor_dirs_file"
-    fi 
-else
-    echo "Could not find $editor_dirs_file in the current directory!"
-    echo "This script will not be able to find any editor configs."
-fi
-found_editor_dirs=()
+load_config_list "$editor_dirs_file" editor_dirs "editor"
 
 hyprland_dirs_file="$config_lists_dir/hyprland_configs.txt"
-# Checks to see if the Hyprland configs list file exists
-if [ -f "$hyprland_dirs_file" ]; then
-    # Checks to see if the mapfile command is available
-    if command -v mapfile >/dev/null 2>&1; then
-        mapfile -t hyprland_dirs < "$hyprland_dirs_file"
-    else
-        hyprland_dirs=()
-        while IFS= read -r line; do
-            hyprland_dirs+=("$line")
-        done < "$hyprland_dirs_file"
-    fi
-else
-    echo "Could not find $hyprland_dirs_file in the current directory!"
-    echo "This script will not be able to find any Hyperland configs."
-    echo
-fi 
-found_hyprland_dirs=()
+load_config_list "$hyprland_dirs_file" hyprland_dirs "Hyprland"
 # === END CONFIG CATEGORIES ===
 
 
 # === START CONFIG DIRECTORY CHECKING ===
-for sub1 in "$config_dir"/*/; do
+all_found_dirs=()
+for dir in "$local_config_dir"/*/; do
     # Remove trailing slash
-    folder="${sub1%/}"
+    dir="${dir%/}"
     # Extract just the folder name
-    folder="${folder##*/}"
-    if [ -d "$repo_config_dir/$folder" ]; then
-        all_found_dirs+=("$folder")
+    dir="${dir##*/}"
+    if [ -d "$repo_config_dir/$dir" ]; then
+        all_found_dirs+=("$dir")
     fi
 done
 
-# Checks for editor configs in the current home directory
-for dir in "${all_found_dirs[@]}"; do
-    # Remove trailing slash
-    folder="${dir%/}"
-    # Extract just the folder name
-    folder="${folder##*/}"
-    
-    for editor_dir in "${editor_dirs[@]}"; do
-        if [[ "$folder" == "$editor_dir" ]]; then
-            found_editor_dirs+=("$folder")
-        fi
-    done
-done
-
-# Checks for Hyprland configs in the current home directory
-for dir in "${all_found_dirs[@]}"; do
-    # Remove trailing slash
-    folder="${dir%/}"
-    # Extract just the folder name
-    folder="${folder##*/}"
-    
-    for hyprland_dir in "${hyprland_dirs[@]}"; do
-        if [[ "$folder" == "$hyprland_dir" ]]; then
-            found_hyprland_dirs+=("$folder")
-        fi
-    done
-done
+# Checks for editor configs in the current config directory
+load_config_dirs all_found_dirs editor_dirs found_editor_dirs
+# Checks for Hyprland configs in the current config directory
+load_config_dirs all_found_dirs hyprland_dirs found_hyprland_dirs
 # === END CONFIG DIRECTORY CHECKING ===
 
 
-# === START CONFIG COPYING USER CONFIRMATION
-if [ ${#found_editor_dirs[@]} -eq 0 ]; then 
-    echo "No editor configs found!"
-else
-    echo "Found editor config directories: ${found_editor_dirs[@]}"
-
-    while true; do
-        read -rp "Copy current editor configs over to repository: (y/N) " copy_editor_config
-        copy_editor_config="${copy_editor_config,,}"
-        if [ -z "$copy_editor_config" ]; then
-            copy_editor_config="n"
-        fi
-
-        case $copy_editor_config in [[yn]) break ;; *) echo "Invalid input, must choose y/n";; esac
-    done
-
-    if [ "$copy_editor_config" = "n" ]; then
-        echo "Skipping copying over editor configs!"
-    fi
-fi
-echo
-
-if [ ${#found_hyprland_dirs[@]} -eq 0 ]; then 
-    echo "No Hyprland configs found!"
-else
-    echo "Found Hyprland config directories: ${found_hyprland_dirs[@]}"
-
-    while true; do
-        read -rp "Copy current Hyperland configs over to repository: (y/N) " copy_hyprland_config
-        copy_hyprland_config="${copy_hyprland_config,,}"
-        if [ -z "$copy_hyprland_config" ]; then
-            copy_hyprland_config="n"
-        fi
-        case $copy_hyprland_config in [[yn]) break ;; *) echo "Invalid input, must choose y/n";; esac
-    done
-    
-    if [ "$copy_hyprland_config" = "n" ]; then
-        echo "Skipping copying over Hyprland configs!"
-    fi
-fi
-echo
-# === END CONFIG COPYING USER CONFIRMATION
-
-
 # === START CONFIG COPYING
-# Enable globbing for file checking
-shopt -s nullglob dotglob
-
-if [ "$copy_editor_config" = "y" ]; then
-    for dir in "${found_editor_dirs[@]}"; do 
-        echo "Grabbing current $dir config..."; 
-        # Grabbing all files in the current dir
-        files=("$config_dir/$dir"/*)
-        # Checks to see if there are no files in the current dir
-        if [ "${#files[@]}" -eq 0 ]; then
-            echo "$dir is empty. Skipping!"
-        else
-            cp --interactive --recursive --verbose "$config_dir/$dir" "$repo_config_dir/"
-            echo "Done!"
-        fi
-    done
-    echo
-fi
-
-if [ "$copy_hyprland_config" = "y" ]; then
-    for dir in "${found_hyprland_dirs[@]}"; do 
-        echo "Grabbing current $dir config..."; 
-        # Grabbing all files in the current dir
-        files=("$config_dir/$dir"/*)
-        # Checks to see if there are no files in the current dir
-        if [ "${#files[@]}" -eq 0 ]; then
-            echo "$dir is empty. Skipping!"
-        else
-            cp --interactive --recursive --verbose "$config_dir/$dir" "$repo_config_dir/"
-            echo "Done!"
-        fi
-    done
-    echo
-fi
-
-# Clean up to abide by best practices
-shopt -u nullglob dotglob
+copy_config_files found_editor_dirs "editor"
+copy_config_files found_hyprland_dirs "Hyprland"
 # === END CONFIG COPYING
 
 echo "Done grabbing configs!"
